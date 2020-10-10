e66a96ff7710bfbe0e790f516cbfdffe90eb9d21
Chris Campbell <chris@change.org>
2020-10-09 21:18:54 -0700

    Test improvements

--- __tests__/get.js
+++ __tests__/get.js
@@ -11,30 +11,34 @@ describe('.get()', () => {
     expect(p.get()).toBe(undefined);
   });
 
-  test("when a property doesn't exist, it returns undefined", () => {
-    expect(p.get('noSuchProp')).toBe(undefined);
+  describe(".get('string')", () => {
+    test("when a property doesn't exist, it returns undefined", () => {
+      expect(p.get('noSuchProp')).toBe(undefined);
+    });
+
+    test('when a property exists, it returns the value', () => {
+      const myObject = {};
+      p = particl({ myKey: myObject });
+
+      expect(p.get('myKey')).toBe(myObject);
+    });
   });
 
-  test('when a property exists, it returns the value', () => {
-    const myObject = {};
-    p = particl({ myKey: myObject });
+  describe('.get(array)', () => {
+    test('when fetching an array of properties, it returns an array of values', () => {
+      p = particl({ a: 1, b: 2, c: 3 });
 
-    expect(p.get('myKey')).toBe(myObject);
-  });
+      expect(p.get(['a', 'b', 'c'])).toEqual([1, 2, 3]);
+    });
 
-  test('when fetching an array of properties, it returns an array of values', () => {
-    p = particl({ a: 1, b: 2, c: 3 });
+    test('when a callback is provided, the results are passed in to the callback', () => {
+      p = particl({ a: 1, b: 2, c: 3 });
 
-    expect(p.get(['a', 'b', 'c'])).toEqual([1, 2, 3]);
-  });
-
-  test('when a callback is provided, the results are passed in to the callback', () => {
-    p = particl({ a: 1, b: 2, c: 3 });
-
-    p.get(['a', 'b', 'c'], (a, b, c) => {
-      expect(a).toBe(1);
-      expect(b).toBe(2);
-      expect(c).toBe(3);
+      p.get(['a', 'b', 'c'], (a, b, c) => {
+        expect(a).toBe(1);
+        expect(b).toBe(2);
+        expect(c).toBe(3);
+      });
     });
   });
 });
--- __tests__/has.js
+++ __tests__/has.js
@@ -20,4 +20,18 @@ describe('.has()', () => {
 
     expect(p.has('a')).toBe(true);
   });
+
+  describe('when called for a list of properties', () => {
+    test('and all of the preperties exist, returns true', () => {
+      p = particl({ a: 1, b: 2 });
+
+      expect(p.has(['a', 'b'])).toBe(true);
+    });
+
+    test("and at least one of the properties doesn't exist, returns false", () => {
+      p = particl({ a: 1 });
+
+      expect(p.has(['a', 'b'])).toBe(false);
+    });
+  });
 });
