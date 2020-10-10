040c164f6de3afeef861566b1f869f600a19b3bc
Chris Campbell <chris@change.org>
2020-10-09 22:54:54 -0700

    Test cleanup

--- __tests__/has.js
+++ __tests__/has.js
@@ -11,24 +11,26 @@ describe('.has()', () => {
     expect(p.has()).toBe(undefined);
   });
 
-  test('when called for a property that is NOT set, return false', () => {
-    expect(p.has('a')).toBe(false);
+  describe('.has(string)', () => {
+    test('when property is NOT set, return false', () => {
+      expect(p.has('a')).toBe(false);
+    });
+
+    test('when property IS set (even to a falsy value), return true', () => {
+      p = particl({ a: 0 });
+
+      expect(p.has('a')).toBe(true);
+    });
   });
 
-  test('when called for a property that IS set (even to a falsy value), return true', () => {
-    p = particl({ a: 0 });
-
-    expect(p.has('a')).toBe(true);
-  });
-
-  describe('when called for a list of properties', () => {
-    test('and all of the preperties exist, returns true', () => {
-      p = particl({ a: 1, b: 2 });
+  describe('.has(array)', () => {
+    test('when all of the preperties exist, returns true', () => {
+      p = particl({ a: 1, b: 0 });
 
       expect(p.has(['a', 'b'])).toBe(true);
     });
 
-    test("and at least one of the properties doesn't exist, returns false", () => {
+    test("when at least one of the properties doesn't exist, returns false", () => {
       p = particl({ a: 1 });
 
       expect(p.has(['a', 'b'])).toBe(false);
