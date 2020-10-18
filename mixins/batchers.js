1308864e60279c410f65500087100f7d3617044c
Chris Campbell <chris@change.org>
2020-10-18 10:55:07 -0700

    Make Accumulator a proper class

--- mixins/batchers.js
+++ mixins/batchers.js
@@ -1,102 +1,95 @@
 module.exports = (api) => {
   const { get, has, off, on } = api;
 
-  const newAccumulator = (key, options = {}, batchesRemaining, cb) => {
-    if (!options.size) options.size = 1;
-    return {
-      key,
-      options,
-      items: [],
-      batchesRemaining,
-      cb,
-    };
-  };
-
-  const accumulate = (accumulator, val) => {
-    const { items, options: { isMatch } } = accumulator;
-    if (!isMatch || isMatch(val)) {
-      items.push(val);
+  class Accumulator {
+    constructor(key, options = {}, batchesRemaining, cb) {
+      if (!options.size) options.size = 1;
+      this.key = key;
+      this.options = options;
+      this.batchesRemaining = batchesRemaining;
+      this.items = [];
+      this.cb = cb;
     }
-  };
 
-  const checkAccumulator = (accumulator) => {
-    const now = Date.now();
-    const {
-      options: { size = 1, timeout },
-      items,
-      batchStart,
-      batchesRemaining,
-      batchTimer,
-    } = accumulator;
-    const fullBatch = items.length >= size;
-    const pastTime = timeout && (batchStart + timeout < now);
-    const anyItems = items.length >= 1;
-    if (batchesRemaining && (fullBatch || (pastTime && anyItems))) {
-      accumulator.items = [];
-      accumulator.batchesRemaining -= 1;
-      if (batchTimer) clearTimeout(batchTimer);
-      // eslint-disable-next-line no-use-before-define
-      if (accumulator.batchesRemaining) resetBatchTimer(accumulator);
-      accumulator.cb(items);
-      return true;
-    }
-    return false;
-  };
-
-  const resetBatchTimer = (accumulator) => {
-    const { options: { timeout } } = accumulator;
-    if (timeout) {
-      accumulator.batchStart = Date.now();
-      accumulator.batchTimer = setTimeout(() => { checkAccumulator(accumulator); }, timeout);
-    }
-  };
-
-  const waitForBatches = (accumulator) => {
-    const { key } = accumulator;
-    resetBatchTimer(accumulator);
-    const listener = (val) => {
-      accumulate(accumulator, val);
-      checkAccumulator(accumulator);
-      if (!accumulator.batchesRemaining) {
-        off(key, listener);
+    accumulate(val) {
+      const { isMatch } = this.options;
+      if (!isMatch || isMatch(val)) {
+        this.items.push(val);
       }
-    };
-    on(key, listener);
-  };
+    }
+
+    check() {
+      const now = Date.now();
+      const {
+        options: { size, timeout },
+        items,
+        batchStart,
+        batchesRemaining,
+        batchTimer,
+      } = this;
+      const fullBatch = items.length >= size;
+      const pastTime = timeout && (batchStart + timeout < now);
+      const anyItems = items.length >= 1;
+      if (batchesRemaining && (fullBatch || (pastTime && anyItems))) {
+        this.items = [];
+        this.batchesRemaining -= 1;
+        if (batchTimer) clearTimeout(batchTimer);
+        // eslint-disable-next-line no-use-before-define
+        if (this.batchesRemaining) this.resetTimer();
+        this.cb(items);
+        return true;
+      }
+      return false;
+    }
+
+    resetTimer() {
+      const { options: { timeout } } = this;
+      if (timeout) {
+        this.batchStart = Date.now();
+        this.batchTimer = setTimeout(() => { this.check(); }, timeout);
+      }
+    }
+
+    waitForBatches() {
+      this.resetTimer();
+      const listener = (val) => {
+        this.accumulate(val);
+        this.check();
+        if (!this.batchesRemaining) {
+          off(this.key, listener);
+        }
+      };
+      on(this.key, listener);
+    }
+  }
 
   return {
     nextBatch(key, options, /* optional */ cb) {
       if (cb) {
-        waitForBatches(newAccumulator(key, options, 1, cb));
+        (new Accumulator(key, options, 1, cb)).waitForBatches();
         return api;
       }
       return new Promise((resolve) => {
-        waitForBatches(newAccumulator(key, options, 1, resolve));
+        (new Accumulator(key, options, 1, resolve)).waitForBatches();
       });
     },
 
     onBatch(key, options, cb) {
-      waitForBatches(newAccumulator(key, options, Infinity, cb));
+      (new Accumulator(key, options, Infinity, cb)).waitForBatches();
       return api;
     },
 
     onceBatch(key, options, /* optional */ cb) {
-      const accumulator = newAccumulator(key, options, 1);
-      if (has(key)) {
-        accumulate(accumulator, get(key));
-      }
+      const accumulator = new Accumulator(key, options, 1);
+      if (has(key)) accumulator.accumulate(get(key));
       if (cb) {
         accumulator.cb = cb;
-        if (!checkAccumulator(accumulator)) {
-          waitForBatches(accumulator);
-        }
+        if (!accumulator.check()) accumulator.waitForBatches();
         return api;
       }
       return new Promise((resolve) => {
         accumulator.cb = resolve;
-        if (!checkAccumulator(accumulator)) {
-          waitForBatches(accumulator);
-        }
+        if (!accumulator.check()) accumulator.waitForBatches();
       });
     },
   };
